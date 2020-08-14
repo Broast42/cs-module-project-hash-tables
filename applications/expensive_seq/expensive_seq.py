9ca96257da209ada69e39077fb9d7581da53ef76
rdowd <rjarrettdowd@yahoo.com>
2020-08-13 20:03:35 -0400

    completed expensive sequence.

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,9 +1,22 @@
 # Your code here
-
+#cache to store already computed results
+cache = {}
 
 def expensive_seq(x, y, z):
     # Your code here
+    if (x,y,z) in cache:
+        return cache[(x,y,z)]
+    #base case if x is less than or equal to zero
+    if x <= 0:
+        #return the sum of y and z
+        return y + z
 
+    #if x is greter than zero
+    if x > 0:
+        value = expensive_seq(x-1,y+1,z) + expensive_seq(x-2,y+2,z*2) + expensive_seq(x-3,y+3,z*3)
+        cache[(x,y,z)] = value
+        return value
+    
 
 
 if __name__ == "__main__":
